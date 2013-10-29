7ae8043890ee5c933be2a4a32b77fe33c496ebee
VanceL <vancelindholm@gmail.com>
2013-10-29 15:18:45 -0700

    Added rule for Buzz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,7 +2,9 @@ package agent;
 public class FizzBuzz {
     public static void main(String[] args) {
         for(int i = 0; i < 100; i++) {
-            System.out.println(i);
+		if (i % 5 == 0){
+            		System.out.println("Buzz");
+		}
         }
     }
 }
