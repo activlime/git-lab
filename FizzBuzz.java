a9265911830c0436fba674a174d26d0f8f0b9881
unknown <choap@usc.edu>
2013-10-29 15:21:23 -0700

    added rule for fizz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,7 +2,8 @@ package agent;
 public class FizzBuzz {
     public static void main(String[] args) {
         for(int i = 0; i < 100; i++) {
-            System.out.println(i);
+	    if(i % 3)
+           	 System.out.println(i);
         }
     }
 }
